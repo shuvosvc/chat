f198634784f2b79f847cf7a0c1d73e5c80503d4d
shuvosvc <5arafatshovo@gmail.com>
2022-09-16 19:35:45 +0600

    feat: chat backend

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+let port = process.env.port || 7000;
+server.listen(port, () => {
+  console.log(`server running in localhost:${port}`);
+});
